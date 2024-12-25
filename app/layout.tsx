c283e6005ded69ae9bc8318c8e7be8b6e2d4dac0
BereketW <bereketsodenoo@gmail.com>
2024-12-25 23:47:08 +0300

    finished projects card

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en overflow-hidden">
       <body className={`${visueltRegular.className} antialiased `}>
         <ThemeProvider
           attribute={"class"}
